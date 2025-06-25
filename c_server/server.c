cb0653f6931a2420a2f29743cb1a59e998127f5c
Andezion <245122@edu.p.lodz.pl>
2025-06-25 16:09:18 +0200

    created structure for server

---
+++ c_server/server.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <signal.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 1024
+
+int main(void)
+{
+    return 0;
+}
