8e51a72f38551d29f9ce6333fe45244b45ab17e6
Andezion <245122@edu.p.lodz.pl>
2025-06-25 16:24:24 +0200

    begin on server

--- c_server/server.c
+++ c_server/server.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,5 +18,33 @@
 
 int main(void)
 {
+    int server_fd, new_fd, port, max_fd;
+    struct sockaddr_in server_address, client_address;
+    socklen_t address_length = sizeof(client_address);
+
+    char buffer[BUFFER_SIZE];
+    int client_sockets[FD_SETSIZE];
+
+    fd_set read_fds, all_fds;
+
+    fprintf(stdout, "Enter port number" );
+    if (scanf("%d", &port) != 1)
+    {
+        fprintf(stderr, "Incorrect input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (port < 1025 || port > 65535)
+    {
+        fprintf(stderr, "Number is incorrect!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        fprintf(stderr, "Failed to create a socket.\n");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
