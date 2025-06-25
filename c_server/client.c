70a53325a32edbbe0c1e872df1091068deedaeca
Andezion <245122@edu.p.lodz.pl>
2025-06-25 13:38:51 +0200

    created path to server

--- c_server/client.c
+++ c_server/client.c
@@ -19,5 +19,29 @@ void buffer(void)
 
 int main(int argc, char *argv[])
 {
+    int number_of_port = 0;
+
+    struct sockaddr_in server_address;
+
+    char ip_address[15];
+    char message_from_client[1024];
+    char message_to_client[1024];
+
+    const int socket_number = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_number == -1)
+    {
+        fprintf(stderr, "Error opening socket\n");
+    }
+
+    fprintf(stdout, "Socket has been created! Socket number: %d\n", socket_number);
+
+    fprintf(stdout, "Enter the IP address of the client: ");
+    fgets(ip_address, 15, stdin);
+
+    buffer();
+
+    server_address.sin_addr.s_addr = inet_addr(ip_address);
+    server_address.sin_family = AF_INET;
+
     return 0;
 }
